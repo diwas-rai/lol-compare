3182eba727cb6a78c0d0b901181cb7d056cb5d53
Diwas Rai <diwasrai38@yahoo.com>
2025-10-30 13:50:55 +0100

    feat: add api endpoint to get a user's last 20 ranked matches by riot id

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 
 app = FastAPI()
 
@@ -29,3 +35,21 @@ def get_data():
         "name": "Sample Data",
         "description": "This data came from your Python backend.",
     }
+
+
+@app.get("/api/matches")
+async def get_matches(gameName: str, tagLine: str):
+    try:
+        player_details = requests.get(
+            f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName.strip()}/{tagLine.strip()}",
+            headers={"X-Riot-Token": RIOT_API_KEY},
+        ).json()
+
+        ranked_match_list = requests.get(
+            f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{player_details['puuid'].strip()}/ids?type=ranked&start=0&count=20",
+            headers={"X-Riot-Token": RIOT_API_KEY},
+        ).json()
+
+        return ranked_match_list
+    except Exception as e:
+        return e
