5b579f45e8c118b8c8f3f22955d1520d884fc0fe
Diwas Rai <diwasrai38@yahoo.com>
2025-11-13 11:19:50 +0000

    build: write configuration for provisioning lambda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
+      code: lambda.DockerImageCode.fromImageAsset("../backend"),
+      memorySize: 3008,
+      timeout: cdk.Duration.seconds(10),
+      architecture: lambda.Architecture.ARM_64,
+      environment: {
+        "NUMBA_CACHE_DIR": "/tmp",
+        "JOBLIB_TEMP_FOLDER": "/tmp"
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const functionUrl = dockerFunc.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.GET, lambda.HttpMethod.POST],
+        allowedHeaders: ["Content-Type"],
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrlValue", {
+      value: functionUrl.url,
+    });
   }
 }
