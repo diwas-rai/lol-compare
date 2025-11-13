9af2a0efd8d60e8d051638bb8543ac51d08a0d7f
Diwas Rai <diwasrai38@yahoo.com>
2025-11-13 14:04:38 +0000

    build: update deployment workflow to use lambda and CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,13 +1,29 @@
+import * as cdk from "aws-cdk-lib";
+import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const imageTag = new cdk.CfnParameter(this, "imageTag", {
+      type: "String",
+      description: "The Docker image tag from the CI pipeline.",
+      default: "latest",
+    });
+
+    const backendRepo = ecr.Repository.fromRepositoryName(
+      this,
+      "BackendRepo",
+      "lol-compare/backend"
+    );
+
     const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
-      code: lambda.DockerImageCode.fromImageAsset("../backend"),
+      code: lambda.DockerImageCode.fromEcr(backendRepo, {
+        tag: imageTag.valueAsString,
+      }),
+
       memorySize: 3008,
       timeout: cdk.Duration.seconds(10),
       architecture: lambda.Architecture.ARM_64,
