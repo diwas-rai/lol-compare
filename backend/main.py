b560560b33320a622d44e9d1decd7b2c459b88ed
Diwas Rai <diwasrai38@yahoo.com>
2025-11-13 11:12:08 +0000

    feat: implement lazy loading for model, scaler, and coords

--- backend/main.py
+++ backend/main.py
@@ -1,49 +1,26 @@
 import logging
+import sys
 from contextlib import asynccontextmanager
-from typing import Iterator, Tuple
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import joblib
-import numpy as np
-from pathlib import Path
 from mangum import Mangum
 
 from routers import *
 
-logger = logging.getLogger("uvicorn.error")
+logging.basicConfig(
+    level=logging.INFO,
+)
 
-BASE_DIR = Path(__file__).resolve().parent
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info("Backend starting... Lifespan initiated.")
+    app.state.model = None
+    app.state.scaler = None
+    app.state.pro_coords = None
 
-    try:
-        logger.info("Loading model...")
-        app.state.model = joblib.load(BASE_DIR / "assets/umap-model.joblib")
-        logger.info("Model loaded successfully.")
-
-        logger.info("Loading scaler...")
-        app.state.scaler = joblib.load(BASE_DIR / "assets/scaler.joblib")
-        logger.info("Scaler loaded successfully.")
-
-        logger.info("Loading pro coords...")
-        pro_coords_zip_obj: Iterator[Tuple[str, np.ndarray]] = joblib.load(
-            BASE_DIR / "assets/umap-coords.joblib"
-        )
-        pro_coords_dict = {
-            player_name: coord.tolist() for player_name, coord in pro_coords_zip_obj
-        }
-        app.state.pro_coords = pro_coords_dict
-        logger.info("Pro coords object loaded successfully.")
-    except Exception as e:
-        logger.error(
-            f"CRITICAL STARTUP FAILURE: Error during loading: {e}", exc_info=True
-        )
-        raise  # Immediately abort startup
-
-    logger.info("Startup complete.")
     yield
 
     logger.info("Application shutting down...")
@@ -75,15 +52,6 @@ app.include_router(transform.router, prefix="/api/transform", tags=["Transform"]
 @app.get("/")
 def read_root():
     return {"message": "Hello from the FastAPI backend!"}
-
-
-@app.get("/api/data")
-def get_data():
-    return {
-        "id": 123,
-        "name": "Sample Data",
-        "description": "This data came from your Python backend.",
-    }
 
 
 handler = Mangum(app, lifespan="on")
