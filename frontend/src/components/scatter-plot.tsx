4deb044cd822870cb12a1993562b9367dbeadbf8
Diwas Rai <diwasrai38@yahoo.com>
2025-11-25 00:07:26 +0000

    fix: hopefully fix this time

--- frontend/src/components/scatter-plot.tsx
+++ frontend/src/components/scatter-plot.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import {
   VictoryAxis,
   VictoryChart,
@@ -18,35 +19,61 @@ const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi");
 const dataPointSize = 7;
 
 export default function ScatterPlot({ domain, data }: GraphProps) {
-  return (
-    <div style={{ touchAction: "none" }}>
-      <VictoryChart
-        domain={domain}
-        theme={VictoryTheme.material}
-        containerComponent={
-          <VictoryZoomVoronoiContainer
-            labels={({ datum }) => datum.key}
-            labelComponent={<VictoryTooltip />}
-            radius={dataPointSize}
-          />
-        }
-      >
-        <VictoryAxis
-          style={{
-            grid: { strokeOpacity: 0 },
-            axis: { strokeOpacity: 0 },
-            axisLabel: { strokeOpacity: 0 },
-            tickLabels: { fillOpacity: 0 },
-            ticks: { strokeOpacity: 0 },
-          }}
-        />
+  // Setup a reference and state to track the container size
+  const ref = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState({ width: 0, height: 0 });
 
-        <VictoryScatter
-          size={dataPointSize}
-          data={data}
-          style={{ data: { opacity: 0.7, fill: ({ datum }) => datum.fill } }}
-        />
-      </VictoryChart>
+  useEffect(() => {
+    const observeTarget = ref.current;
+    if (!observeTarget) return;
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      if (!entries || entries.length === 0) return;
+      const { width, height } = entries[0].contentRect;
+      setSize({ width, height });
+    });
+
+    resizeObserver.observe(observeTarget);
+    return () => {
+      if (observeTarget) resizeObserver.unobserve(observeTarget);
+    };
+  }, []);
+
+  return (
+    <div ref={ref} className="w-full h-full" style={{ touchAction: "none" }}>
+      {size.width > 0 && (
+        <VictoryChart
+          width={size.width}
+          height={size.height}
+          domain={domain}
+          theme={VictoryTheme.material}
+          containerComponent={
+            <VictoryZoomVoronoiContainer
+              labels={({ datum }) => datum.key}
+              labelComponent={<VictoryTooltip />}
+              radius={dataPointSize}
+              allowZoom={true}
+              allowPan={true}
+            />
+          }
+        >
+          <VictoryAxis
+            style={{
+              grid: { strokeOpacity: 0 },
+              axis: { strokeOpacity: 0 },
+              axisLabel: { strokeOpacity: 0 },
+              tickLabels: { fillOpacity: 0 },
+              ticks: { strokeOpacity: 0 },
+            }}
+          />
+
+          <VictoryScatter
+            size={dataPointSize}
+            data={data}
+            style={{ data: { opacity: 0.7, fill: ({ datum }) => datum.fill } }}
+          />
+        </VictoryChart>
+      )}
     </div>
   );
 }
